bbfb94cfaa3be6a2cf6379598eadc6a9ad355055
David Todd <c0de@c0defox.es>
2019-05-27 23:56:01 -0500

    update libraries

--- gen_statics_bundles.py
+++ gen_statics_bundles.py
@@ -2,6 +2,7 @@
 import itertools, os, re, sys
 from hashlib import sha1
 
+# This is pretty bad... please rewrite me
 
 def cat(filepath):
     with open(filepath, 'rb') as f:
@@ -16,33 +17,33 @@ def sed(filepath, pattern, value):
 
 
 print('Generating CSS bundle')
-short_hash = sha1(cat('static/main.css')).hexdigest()[:7]
+short_hash = sha1(cat('static/main.css') + cat('static/csslibs/uikit-3.1.5.min.css')).hexdigest()[:7]
 sed('templates/base.html', '-SHORTSHA1-[a-z0-9]+.css', '-SHORTSHA1-{}.css'.format(short_hash))
 css_bundle_filepath = 'static/bundle-SHORTSHA1-{}.css'.format(short_hash)
 files_created = not os.path.exists(css_bundle_filepath)
 with open(css_bundle_filepath, 'wb') as bundle:
-    bundle.write(cat('static/csslibs/uikit-2.27.4.min.css'))
+    bundle.write(cat('static/csslibs/uikit-3.1.5.min.css'))
     bundle.write(cat('static/csslibs/solarized-highlight.css'))
     bundle.write(cat('static/csslibs/tipuesearch.css'))
     bundle.write(cat('static/main.css'))
 
 print('Generating JS bundle')
-short_hash = sha1(cat('static/js/social.js') + cat('static/js/filter-tags.js')).hexdigest()[:7]
+short_hash = sha1(cat('static/jslibs/uikit-3.1.5.min.js') + cat('static/jslibs/jquery-3.4.1.min.js')).hexdigest()[:7]
 sed('templates/base.html', '-SHORTSHA1-[a-z0-9]+.js', '-SHORTSHA1-{}.js'.format(short_hash))
 for SHARE, MG_FILTER_TAGS in itertools.product(range(2), repeat=2):
     js_bundle_filepath = 'static/bundle-SHARE-{}-MG_FILTER_TAGS-{}-SHORTSHA1-{}.js'.format(
             SHARE, MG_FILTER_TAGS, short_hash)
     files_created |= not os.path.exists(js_bundle_filepath)
     with open(js_bundle_filepath, 'wb') as bundle:
-        bundle.write(cat('static/jslibs/html5shiv-3.7.2.min.js'))
-        bundle.write(cat('static/jslibs/jquery-1.10.2.min.js'))
-        bundle.write(cat('static/jslibs/uikit-2.27.4.min.js'))
-        bundle.write(cat('static/jslibs/lazysizes-4.0.0-rc3.min.js'))
-        bundle.write(cat('static/jslibs/lazysizes-4.0.0-rc3.noscript.min.js'))
+        bundle.write(cat('static/jslibs/html5shiv-3.7.3.min.js'))
+        bundle.write(cat('static/jslibs/jquery-3.4.1.min.js'))
+        bundle.write(cat('static/jslibs/uikit-3.1.5.min.js'))
+        bundle.write(cat('static/jslibs/uikit-icons-3.1.5.min.js'))
+        bundle.write(cat('static/jslibs/lazysizes-5.0.0.min.js'))
         bundle.write(cat('static/jslibs/tipuesearch.js'))
         bundle.write(cat('static/jslibs/tipuesearch_set.js'))
         if SHARE:
-            bundle.write(cat('static/jslibs/jquery.sticky-kit.js'))
+            bundle.write(cat('static/jslibs/jquery.sticky-kit-1.1.2.min.js'))
             bundle.write(cat('static/js/social.js'))
         if MG_FILTER_TAGS:
             bundle.write(cat('static/js/filter-tags.js'))
